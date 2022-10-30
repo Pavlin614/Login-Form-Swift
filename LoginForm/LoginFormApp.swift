68c44a83ccba6bd8ae82f27aad61e7da0504a771
Pavlin Dimitrov <pavlin.dimitrov@paysafe.com>
2022-10-30 13:09:55 +0200

    Initial Commit

---
+++ LoginForm/LoginFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginFormApp.swift
+//  LoginForm
+//
+//  Created by Pavlin Dimitrov on 30.10.22.
+//
+
+import SwiftUI
+
+@main
+struct LoginFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
