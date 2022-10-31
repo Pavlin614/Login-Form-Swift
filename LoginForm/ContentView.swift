c55b10e5ea0721a713f8ac5b6d036a450c8dfcb3
Pavlin Dimitrov <pavlin.dimitrov@paysafe.com>
2022-10-31 08:56:11 +0200

    initial commit

--- LoginForm/ContentView.swift
+++ LoginForm/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var username = ""
+    @State var password = ""
+    @State var backgroundColor: Color = .white
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+            VStack (alignment: .center, spacing: 10){
+                Spacer()
+                Text("Login Form").foregroundColor(.black).font(Font.title)
+                Image(systemName: "globe").foregroundColor(.black).imageScale(.large)
+                
+                HStack(alignment: .center, spacing: 10){
+                    Text("Username").foregroundColor(.black).font(Font.system(size: 20))
+                    TextField("username", text: $username, prompt: Text("Enter username:"))
+                }
+                
+                HStack(alignment: .center, spacing: 10){
+                    Text("Password").foregroundColor(.black).font(Font.system(size: 20))
+                    SecureField("password", text: $password, prompt: Text("Enter passord:")).disableAutocorrection(true)
+                }
+                
+                Button("Login") {
+                    if username=="test" && password == "test" {
+                        backgroundColor = Color.green
+                    }
+                    else{
+                        backgroundColor = Color.red
+                    }
+                }.buttonStyle(.bordered).tint(.black)
+                Spacer()
+            }
+            .padding()
+            .background(backgroundColor)
     }
 }
 
